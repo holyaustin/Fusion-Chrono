38dfc73ea7b2c27fb28465d92801acda685d2f40
Augustine Onuora <holyaustin@yahoo.com>
2025-07-27 23:05:05 +0100

    mock erc20  deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,7 @@ const config: HardhatUserConfig = {
     networks: {
     etherlinkTestnet: {
       url: "https://node.ghostnet.etherlink.com",
-      accounts: ["e14d8ec28319e6529455e87a7bb4d158d06e3b6038bdf6962364c2073b1bb75b"]
+      accounts: [""]
     }
   }
 };
